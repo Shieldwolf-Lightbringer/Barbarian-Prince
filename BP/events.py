632f90e05aea1b2461d285f66d2a86ec58aa253d
Randall <randall.lux@gmail.com>
2024-03-27 12:08:34 -0400

    fixed usurper event to trigger north of Tragoth river

--- BP/events.py
+++ BP/events.py
@@ -4,7 +4,7 @@ from hexmap import overland_map
 from random import choice, randint
 
 # Not yet fully implemented
-def e002(party, hex, console):
+def e002(party, console, hex):
     north_tragoth_river = [(1,1),(2,1),(3,1),(3,2),(4,1),(5,1),(5,2),(6,1),(7,1),(8,1),(9,1),
                            (10,1),(11,1),(12,1),(13,1),(15,1),(16,1),(17,1),(18,1),(19,1),(20,1),]
     if hex in north_tragoth_river:
@@ -13,11 +13,12 @@ def e002(party, hex, console):
             event_roll += 1
         if event_roll >= 1:
             thugs = randint(1,6)
-            enemy_party = []
+            usurper_party = []
             console.display_message(f'{thugs} Mercenary thugs, dressed by the usurpers as their royal guardsmen, are riding towards you!')
             for _ in range(thugs):
-                enemy_party.append(characters.Character(title='Usurper Thug', combat_skill=5, endurance=4, wealth_code=4, mounted=True))
+                usurper_party.append(characters.Character(title='Usurper Thug', combat_skill=5, endurance=4, wealth_code=4, mounted=True))
             console.display_message('You have the option to negotiate, evade, or fight them.')
+            game_actions.combat(party, usurper_party, console)
 
 
 def e003(party, console): # Swordsman on horse
